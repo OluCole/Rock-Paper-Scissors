5970bfd72b68e86946bb9438ada19f1e5697afc9
OluCole <coleseyi19@gmail.com>
2024-11-19 08:31:49 -0500

    Update and rename Intergration1.py to Iteration1.py
    Version 1: Created user input function

--- Intergration1.py
+++
@@ -1,5 +0,0 @@
-def get_player_choice(Rock, Paper, Scissors):
-  return Rock + Paper + Scissors
-user_choice = input("Choose Rock, Paper, or Scissors?: ")
-print()
-print(user_choice)
---
+++ Iteration1.py
@@ -0,0 +1,4 @@
+print("Time to play Rock, Paper, or Scissors!><!")
+def get_player_choice():
+  user_choice = input("Choose Rock, Paper, or Scissors?: ")
+  return user_choice
