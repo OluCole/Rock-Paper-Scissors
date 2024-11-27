c18779fe79290d84a59ea9a4980ff2f467e447c1
mari 0 <0somethingsomething0@gmail.com>
2024-11-27 08:22:53 -0500

    Update Iteration1.py

--- Iteration1.py
+++ Iteration1.py
@@ -1,5 +1,4 @@
 print("Time to play Rock, Paper, or Scissors!><!")
 def get_player_choice():
-  """Obtains and returns user's choice of rock, paper, or scissors"""
   user_choice = input("Choose Rock, Paper, or Scissors?: ")
   return user_choice
